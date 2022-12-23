b073c348158f07d7473d7384e524e8d43ef7e686
Elisabeth Hölscher <elisabeth.hoelscher@gmail.com>
2022-12-23 15:39:11 +0100

    Use PostGis instead of PostGreSQL

--- api/config.py
+++ api/config.py
@@ -3,7 +3,7 @@ import os
 server_url = os.environ.get("SERVER_URL", "localhost:5001")
 
 postgres_db = os.environ.get("POSTGRES_DB", "cut_re3_dev")
-postgres_host = os.environ.get("POSTGRES_HOST", "db")
+postgres_host = os.environ.get("POSTGRES_HOST", "postgis")
 postgres_user = os.environ.get("POSTGRES_USER", "postgres")
 postgres_password = os.environ.get("POSTGRES_PASSWORD", "postgres")
 postgres_port = os.environ.get("POSTGRES_PORT", 5432)
