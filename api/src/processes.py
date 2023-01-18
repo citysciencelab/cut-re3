51a03b07ddf2f32ed7544108111e12f4be84f07b
Elisabeth Hölscher <elisabeth.hoelscher@gmail.com>
2023-01-18 13:01:55 +0100

    Add exclude certain processes.

--- api/src/processes.py
+++ api/src/processes.py
@@ -3,6 +3,8 @@ import logging
 import base64
 import json
 import yaml
+import traceback
+
 
 PROVIDERS = yaml.safe_load(open('./configs/providers.yml'))
 
@@ -18,22 +20,26 @@ def all_processes():
           headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
         )
 
-      if response.ok:
-        results = response.json()
-        for process in results["processes"]:
-          process_id = {
-            "process_id": process['id'],
-            "provider_prefix": prefix
-          }
-          process["id"] = base64.urlsafe_b64encode(json.dumps(process_id).encode()).decode()
-
-        processes += results["processes"]
-      else:
+      if not response.ok:
         logging.error(f"Processes could not be retrieved from provider {p['url']}!!! {response.status_code}: {response.reason}")
+        continue
+
+      results = response.json()
+      for process in results["processes"]:
+        if process["id"] in p["exclude"]:
+          continue
+
+        process_id = {
+          "process_id": process['id'],
+          "provider_prefix": prefix
+        }
+        process["id"] = base64.urlsafe_b64encode(json.dumps(process_id).encode()).decode()
+        processes.append(process)
 
     except Exception as e:
       logging.error(f"Could not connect to {p['url']}:")
       logging.error(e)
       logging.error("Please fix entries in providers.yml!!!")
+      traceback.print_exc()
 
   return { "processes": processes }
